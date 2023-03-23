22df8ca4376bb1f17fd7a233dfbabac91c6e0fa8
sujungleeml <sujunglee.ml@outlook.com>
2023-03-23 13:18:13 +0900

    20230323 수정 변경사항

--- 이수정/Python/create_db.py
+++ 이수정/Python/create_db.py
@@ -5,14 +5,16 @@ import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+
+
 # create a database (database name is mimic3.db)
-conn = sqlite3.connect('mimic3.db')
-csv_list = [csv_file for csv_file in os.listdir('C:/Data/MIMIC4/mimic-iv-2.2/') if csv_file.endswith('.csv')]
+conn = sqlite3.connect('mimic4.db')
+csv_list = [csv_file for csv_file in os.listdir('C:/Data/MIMIC4/mimic-iv-2.2_unzip/') if csv_file.endswith('.csv')]
 
 # load all csvs in mimic-iii-clinical-database-1.4 folder and save them in a database
 for csv_file in csv_list:
     print('Loading ' + csv_file)
-    df = pd.read_csv('C:/Data/MIMIC4/mimic-iv-2.2/' + csv_file, chunksize=1000000, iterator=True, low_memory=False)
+    df = pd.read_csv('C:/Data/MIMIC4/mimic-iv-2.2_unzip/' + csv_file, chunksize=1000000, iterator=True, low_memory=False)
     for chunk in tqdm(df):
         chunk.to_sql(csv_file[:-4], conn, if_exists='append', index=False)
 
