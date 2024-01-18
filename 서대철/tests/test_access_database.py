351928c04eaa29281193ad81ef0356b9a4cd6b25
SDC <dchlseo@gmail.com>
2024-01-18 18:38:17 +0900

    Fixed json param name

--- 서대철/tests/test_access_database.py
+++ 서대철/tests/test_access_database.py
@@ -3,7 +3,7 @@ import os
 import unittest
 
 # Adjust the path to include the /src directory
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src', 'data_extraction')))
 
 from access_database import main, load_config
 
