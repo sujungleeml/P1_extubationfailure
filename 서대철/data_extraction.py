2c9fd72cb8a0784ada13382e46a798d2d1583c4f
SDC <dchlseo@gmail.com>
2024-01-21 13:48:26 +0900

    Modifeid output strategy

--- 서대철/data_extraction.py
+++ 서대철/data_extraction.py
@@ -1,10 +1,10 @@
 import os
 import pandas as pd
 from src.data_extraction.access_database import main as access_database_main
-from src.data_extraction.filter_adult_patients import filter_adult_patients, merge_patient_admissions
+import src.data_extraction.filter_adult_patients as fap
 from src.data_extraction.filter_ventilation_events import process_ventilation_data
 
-def main():
+def main(outputs='all'):
     # DB 접속 후 데이터 메모리로 저장
     config_file_path = './config.json'
     dataframes = access_database_main(config_file_path)
@@ -14,21 +14,34 @@ def main():
     admissions = dataframes['admissions']
     intubation_all = dataframes['intubation_all']
     extubation_all = dataframes['extubation_all']
+    icustays = dataframes['icustays']
 
-    # 데이터 처리
+    ## 데이터 처리: 응급병동 환자 데이터
     # 성인환자 데이터 필터링
-    adults_pat = filter_adult_patients(patients)
-    adults_hadm = merge_patient_admissions(adults_pat, admissions)
+    adults_pat = fap.filter_adult_patients(patients)   # 18세 이상 필터링
+    adults_hadm = fap.merge_patient_admissions(adults_pat, admissions)   # patient, admissions 테이블 결합
+    adults_hadm = fap.remove_missing_hadm(adults_hadm)   # 입원정보(hadm_id) 없는 행 삭제
+    adults_icu = fap.merge_with_icu(adults_hadm, icustays)   # icu (응급병동) 테이블 결합
+    adults_icu = fap.remove_missing_icu_stays(adults_icu)   # icu 입원정보(stay_id) 없는 행 삭제
 
+    ## 데이터 처리: 삽관/발관 데이터
     # 삽관/발관 데이터 필터링 및 처리
     intubation_data = process_ventilation_data(intubation_all, 'intubationtime', 'int_itemid', 'intubation')
     extubation_data = process_ventilation_data(extubation_all, 'extubationtime', 'ext_itemid', 'extubation')
 
     # 데이터 저장
     output_dir = './outputs'
-    adults_hadm.to_csv(os.path.join(output_dir, 'adults_hadm.csv'), index=False)
-    intubation_data.to_csv(os.path.join(output_dir, 'intubation_data.csv'), index=False)
-    extubation_data.to_csv(os.path.join(output_dir, 'extubation_data.csv'), index=False)
+
+    # 데이터 저장
+    if outputs == 'all':
+        adults_icu.to_csv(os.path.join(output_dir, 'adults_icu.csv'), index=False)
+        intubation_data.to_csv(os.path.join(output_dir, 'intubation_data.csv'), index=False)
+        extubation_data.to_csv(os.path.join(output_dir, 'extubation_data.csv'), index=False)
+    elif outputs == 'patients':
+        adults_icu.to_csv(os.path.join(output_dir, 'adults_icu.csv'), index=False)
+    elif outputs == 'ventilations':
+        intubation_data.to_csv(os.path.join(output_dir, 'intubation_data.csv'), index=False)
+        extubation_data.to_csv(os.path.join(output_dir, 'extubation_data.csv'), index=False)   
 
     print("Data extraction and processing complete. Files saved.")
 
--- 서대철/src/data_extraction/filter_adult_patients.py
+++ 서대철/src/data_extraction/filter_adult_patients.py
@@ -17,3 +17,18 @@ def merge_patient_admissions(adults, admissions):
         >> select("subject_id", "gender", "anchor_age", "hadm_id", "admittime", "dischtime", "deathtime")
     
     return adults_hadm >> mask(adults_hadm.hadm_id.notnull())
+
+
+def remove_missing_hadm(adults_hadm):
+    """입원정보(hadm_id) 없는 행 삭제"""
+    return adults_hadm >> mask(adults_hadm.hadm_id.notnull())
+
+
+def merge_with_icu(adults_hadm, icustays):
+    """icu (응급병동) 테이블 결합"""
+    return adults_hadm >> left_join(icustays, by=("subject_id", "hadm_id"))
+
+
+def remove_missing_icu_stays(adults_icu):
+    """icu 입원정보(stay_id) 없는 행 삭제)"""
+    return adults_icu >> mask(adults_icu.stay_id.notnull())
